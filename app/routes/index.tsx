c968dea7dda7894be3c1ef73fbcacfc12c523007
vic13pg <57153795+vic13pg@users.noreply.github.com>
2025-03-24 19:18:26 +0000

    sesion style dictionary

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,11 +2,11 @@ import type { Route } from './+types/index';
 import HomePage from '../home/home';
 
 export async function loader () {
-  //const texts = await import('../build/es/texts.json');
+  const texts = await import('../texts.json');
 
-  //return { texts };
+  return { texts };
 
-  return { texts: {} };
+
 }
 
 export default function Index({ loaderData } : Route.ComponentProps) {
