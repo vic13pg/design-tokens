882dda210e8dd5be1d95387cda799805d70b301c
vic13pg <57153795+vic13pg@users.noreply.github.com>
2025-03-31 16:18:30 +0000

    wip

--- app/root.tsx
+++ app/root.tsx
@@ -25,7 +25,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap",
   },
 ];
 
--- build.js
+++ build.js
@@ -1,4 +1,38 @@
+import StyleDictionary from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
+import ThemesLoader from 'sd-themes-loader';
+import { platform } from 'os';
+
+register(StyleDictionary, {
+    withSDBuiltins: false,
+});
+
+const loader = ThemesLoader(StyleDictionary);
+
 async function run() {
+    const themes = await loader.load('/tokens');
+
+    const globalTheme = themes.getThemeByName('global');
+
+    const config = {
+        platforms: {
+            web: {
+                files: [
+                    {
+                        destination: 'app/build/global/variables.css',
+                        format:'css/variables',
+                    }
+                ],
+                transforms:[
+                    'name/kebab',
+                    'ts/resolveMath',
+                    'size/pxToRem'
+                ]
+            }
+        }
+    };
+
+    globalTheme.addConfig(config).build();
 
 }
 
